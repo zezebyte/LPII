1633a084809948c18b7bbd6fe0a9229a6bc9a05e
eLiTe <eLiTe@eLiTePC>
2014-04-23 17:43:09 +0100

    types fusion

--- types.h
+++ types.h
@@ -1,4 +1,38 @@
-#include "Rolo.h"
+#include "stack.h"
+#define STRG 80
+#define MAX 100
+
+typedef struct armazem *ApArmazem;
+typedef struct no *ApNo;
+
+typedef struct{
+    int dia, mes, ano;
+}Data;
+
+typedef struct{
+    char codigo[7];
+    Data data;
+    Stack pilharolos;
+    int open;
+}Pack;
+
+typedef struct{
+    char codigo[10], descr[STRG];
+    int enc, qualid;
+    float comp;
+}Rolo;
+
+typedef struct armazem{
+    Rolo rolosarmazem[MAX];
+    Pack packsarmazem[MAX];
+    int cont_rolos;
+    int cont_packs;
+}Armazem;
+
+typedef struct no {
+	comp elem;
+	ApNo next;
+}No;
 
 typedef Rolo comp;
 

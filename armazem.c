c7ec6f6022f341ae9803ea365d00e2b0d9ab298c
eLiTe <eLiTe@eLiTePC>
2014-04-23 18:36:34 +0100

    a

--- armazem.c
+++ armazem.c
@@ -7,10 +7,25 @@ void InitArm(ApArmazem armaz){
     armaz->cont_rolos=0;
 }
 
-int ProcuraCodPack(char, ApArmazem){
+int ProcuraCodRolo(char codigo[], ApArmazem armaz){
     int pos=0;
     
-    while()
+    while(pos<armaz->cont_rolos && (strcmp(codigo,armaz->rolosarmazem[pos].codigo))!=0)
+        pos++;
+    if(pos!=armaz->cont_rolo)s
+        return pos;
+    else
+        return -1;
     
+}
+
+int ProcuraCodPack(char codigo[], ApArmazem armaz){
+    int pos=0;
     
+    while(pos<armaz->cont_packs && (strcmp(codigo,armaz->packsarmazem[pos].codigo))!=0)
+        pos++;
+    if(pos!=armaz->cont_packs)s
+        return pos;
+    else
+        return -1;
 }

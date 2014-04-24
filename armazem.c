550eee521db4a16ab8736de6a03642531938d587
zezebyte <zeze_byte@hotmail.com>
2014-04-24 08:49:40 +0100

    Update armazem.c

--- armazem.c
+++ armazem.c
@@ -29,3 +29,32 @@ int ProcuraCodPack(char codigo[], ApArmazem armaz){
     else
         return -1;
 }
+int ProcuraCodigoRoloEmPacks(ApArmazem armaz, char codigo[]){
+    int i;
+    
+    for(i=0; i<armaz->cont_packs-1; i++){
+        if(SearchCodS(&(armaz->packsarmazem[i]), codigo))
+            return 1; 
+    }
+    return 0;
+}
+
+void ReceberRolo(pQueue Rolo, ApArmazem armaz){
+    ApNo aux;
+    char str[STRG];
+    
+    if(FullQ(Rolo)=0){
+        aux=malloc(sizeof(No));
+        printf("Adicionar Rolo\n");
+        printf("Introduza a descricao do produto: ");
+        fgets(aux->elem.descr, sizeof(aux->elem.descr), stdin);
+        printf("Introduza a encomenda do porduto: ");
+        fgets(str, sizeof(str), stdin);
+        sscanf(str, "%d", aux->elem.enc);
+        Enqueue(Rolo, aux);
+        printf("Rolo introduzido com sucesso!\n");
+    }else{
+        printf("Buffer da seccao de acabamentos cheio!\n")
+    }
+}
+

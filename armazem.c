2c1c865c93678ee755b4821f88ec99f6c8cc2b56
zezebyte <zeze_byte@hotmail.com>
2014-04-24 09:47:26 +0100

    Update armazem.c

--- armazem.c
+++ armazem.c
@@ -58,3 +58,100 @@ void ReceberRolo(pQueue Rolo, ApArmazem armaz){
     }
 }
 
+void AdRoloArm(pQueue ap_queue, ApArmazem armaz){
+    ApNo aux;
+    char str[STRG];
+    
+    if(EmptyQ(ap_queue)==0){
+        aux=Dequeue(ap_queue);
+        printf("Introduza o codigo do Rolo: ");
+        fgets(str, sizeof(str), stdin);
+        if(strlen(str)<11){
+            if(ProcuraCodRolo(str,armaz)==-1 && ProcuraCodigoRoloEmPacks(armaz, str)==0){
+                strcpy(aux->elem.codigo, str);
+                printf("Introduza o comprimento do rolo: ");
+                fgets(str, sizeof(str), stdin);
+                sscanf(str, "%f", aux->elem.comp);
+                if(aux->elem.comp>0.0){
+                    printf("Introduza a qualidade do rolo: ");
+                    fgets(str, sizeof(str), stdin);
+                    sscanf(str, "%d", &(aux->elem.enc));
+                    armaz->rolosarmazem[armaz->cont_rolos]=aux->elem;
+                    free(aux);
+                    armaz->cont_rolos= armaz->cont_rolos+1;
+                    printf("Rolo adicionado ao armazem com sucesso\n"); 
+                }else{
+                    Enqueue(aux, ap_queue);
+                    printf("Os rolos tem que ter o comprimento positivo!\n");
+                }
+            }else{
+                Enqueue(aux, ap_queue);
+                printf("Já existe um rolo com esse codigo!\n");
+            }
+        }else{
+            Enqueue(aux, ap_queue);
+            printf("O codigo so pode ter ate 10 caracteres!\n");
+        }
+    }else{
+        Enqueue(aux, ap_queue);
+        printf("Nao ha rolos para adicionar!\n");
+    }
+}
+
+void RemoverRolo(ApArmazem armaz){
+    char cod[10];
+    int pos, i;
+    
+    printf("Inttroduza o codigo do rolo que pretende remover: ");
+    fgets(str, sizeof(str), stdin);
+    pos=ProcuraCodRolo(cod, armaz);
+    if(pos!=-1){
+        armaz->cont_rolos=armaz->cont_rolos-1;
+        for(i=pos; i<armaz->cont_rolos-1; i++){
+            armaz->rolosarmazem[i]=armaz->rolosarmazem[i+1];
+        }
+        printf("Rolo apagado com sucesso!\n");
+    }else{
+        printf("Nao existe um rolo com esse codigo para apagar!\n");
+    }
+}
+
+void AlterarRolos(ApArmazem armaz){
+    int ind;
+    float aux;
+    char str[STRG];
+    
+    if(armaz->cont_rolos!=0){
+        printf("Alteracao de rolos\n");
+        printf("Introduza o codigo do rolo a alterar: ");
+        fgets(str, sizeof(str), stdin);
+        if(strlen(str)<11){
+            ind=ProcuraCodRolo(str, armaz);
+            if(ind!=-1){
+                printf("Introduza o comprimento do rolo: ");
+                fgets(str, sizeof(str), stdin);
+                sscanf(str, "%f", &aux);
+                if(aux>0.0){
+                    armaz->rolosarmazem[ind].comp=aux;
+                    printf("Introduza a qualidade do rolo: ");
+                    fgets(str, sizeof(str), stdin);
+                    scanf(str, "%d", &(armaz->rolosarmazem[ind].qualid));
+                    printf("Introduza a descricao do produto: ");
+                    fgets(armaz->rolosarmazem[ind].descr, sizeof(armaz->rolosarmazem[ind].descr), stdin);
+                    printf("Introduza a encomenda do produto: ");
+                    fgets(str, sizeof(str), stdin);
+                    scanf(str, "%d", &(armaz->rolosarmazem[ind].enc));
+                    printf("Rolo editado com sucesso!\n");
+                }else{
+                    printf("Os rolos tem de ter comprimento positivo!\n");
+                }
+            }else{
+                printf("Nao existe um rolo com esse codigo!\n");
+            }
+        }else{
+           printf("O codigo so pode ter ate 10 caracteres!\n"); 
+        }
+    }else{
+        printf("Nao há rolos para alterar!\n");
+    }
+}
